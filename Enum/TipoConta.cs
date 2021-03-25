f2d1053464c628ba7f064fff827b993ed79b175f
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-03-25 11:08:36 -0300

    Definindo atributos e métodos

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace DIO_bank
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica  = 2
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DIO_bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Samuel Lucas");
+
+            Console.WriteLine(minhaConta.ToString());
+        }
+    }
+}
