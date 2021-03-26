355ed342ab801b1ccbeba618d79a1ad5ff2dbcfb
Samuel-Lucas <samuel.moreira10@outlook.com>
2021-03-25 21:32:59 -0300

    desenvolvendo menu para escolha de métodos

--- Program.cs
+++ Program.cs
@@ -1,14 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO_bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Samuel Lucas");
+            string opcaoUsuario = ObterOpcaoUsuario();
 
-            Console.WriteLine(minhaConta.ToString());
+            while(opcaoUsuario.ToUpper() != "X") {
+                switch(opcaoUsuario) {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                     //   Transferir();
+                        break;
+                    case "4":
+                     //   Sacar();
+                        break;
+                    case "5":
+                     //   Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços");
+            Console.ReadLine();
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar Contas");
+
+            if(listContas.Count == 0) {
+                Console.WriteLine("Nenhuma conta cadastrada");
+                return;
+            }
+
+            for(int i = 0; i < listContas.Count; i++) {
+                Conta conta = listContas[i];
+                Console.Write("#{0} - ", i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserirConta() {
+            Console.WriteLine("Inserir nova conta");
+
+            Console.Write("Digite 1 para conta Física ou 2 para conta jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+             Console.Write("Digite o crédito inicial: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta, //Converte entradaTipoConta para o enum
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+            listContas.Add(novaConta);
+        }
+
+        private static string ObterOpcaoUsuario() {
+            Console.WriteLine();
+            Console.WriteLine("DIO bank a seu dispor !");
+            Console.WriteLine("Informe a opção desejada");
+
+            Console.WriteLine("1 - Listar contas");
+            Console.WriteLine("2 - Inserir nova conta");
+            Console.WriteLine("3 - Transferir");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
         }
     }
 }
